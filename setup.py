59f575b7cbbdbcee040f8df0583eac15eff7bdcf
PabloViniegra <pablovpmadrid@gamil.com>
2021-02-26 12:46:37 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='whatsappMessage',
+    version='1.0.0',
+    packages=['pythonProject'],
+    url='http://whatsapp_message.com',
+    license='Free Common',
+    author='pablo',
+    author_email='pablovpmadrid@gmail.com',
+    description='script to send whatsapp\'s messages'
+)
